fb2aea19310ca3402b496cccf3e1d2476ea742fa
micha <86150459+ptichkatop@users.noreply.github.com>
2021-06-19 13:27:07 +0300

    Initial commit

---
+++ js/size/size.js
@@ -0,0 +1,66 @@
+const image = document.querySelector('.image');
+
+image.addEventListener('mouseenter', function(){
+    image.classList.add('hover');
+});
+image.addEventListener('mouseleave', function(){
+    image.classList.remove('hover');
+})
+
+image.addEventListener('mouseenter', function(){
+    image.classList.add('hover');
+});
+image.addEventListener('mouseleave', function(){
+    image.classList.remove('hover');
+})
+
+
+const heading_block1 = document.querySelector('.heading_block1');
+
+heading_block1.addEventListener('mouseenter', function(){
+    heading_block1.classList.add('hover');
+});
+heading_block1.addEventListener('mouseleave', function(){
+    heading_block1.classList.remove('hover');
+})
+
+heading_block1.addEventListener('mouseenter', function(){
+    heading_block1.classList.add('hover');
+});
+heading_block1.addEventListener('mouseleave', function(){
+    heading_block1.classList.remove('hover');
+})
+
+
+const heading_block2 = document.querySelector('.heading_block2');
+
+heading_block2.addEventListener('mouseenter', function(){
+    heading_block2.classList.add('hover');
+});
+heading_block2.addEventListener('mouseleave', function(){
+    heading_block2.classList.remove('hover');
+})
+
+heading_block2.addEventListener('mouseenter', function(){
+    heading_block2.classList.add('hover');
+});
+heading_block2.addEventListener('mouseleave', function(){
+    heading_block2.classList.remove('hover');
+})
+
+
+const heading_block3 = document.querySelector('.heading_block3');
+
+heading_block3.addEventListener('mouseenter', function(){
+    two.heading_block3.add('hover');
+});
+heading_block3.addEventListener('mouseleave', function(){
+    heading_block3.classList.remove('hover');
+})
+
+heading_block3.addEventListener('mouseenter', function(){
+    heading_block3.classList.add('hover');
+});
+heading_block3.addEventListener('mouseleave', function(){
+    heading_block3.classList.remove('hover');
+})
